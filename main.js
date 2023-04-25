7318e74704f6d32df1ad42c7850ebd6633d29f65
zxhd <zxhd863943427@outlook.com>
2023-04-25 17:16:28 +0800

    植入默认配色方案

--- main.js
+++ main.js
@@ -8,6 +8,68 @@ const defaultConfig = {
     },
 }
 
+const defaultSchemes = {
+    "name": "default",
+    "schemes": {
+        "light":{
+            "color1": "rgb(97, 26, 21)",
+            "color2": "rgb(102, 60, 0)",
+            "color3": "rgb(13 60 97)",
+            "color4": "rgb(30 70 32)",
+            "color5": "#5f6368",
+            "color6": "#3575f0",
+            "color7": "#f3a92f",
+            "color8": "#d23f31",
+            "color9": "#f5539e",
+            "color10": "#944194",
+            "color11": "#65b84d",
+            "color12": "#f5822e",
+            "color13": "#ffffff",
+            "background1": "#f5d1cf",
+            "background2": "#ffe8c8",
+            "background3": "#d6eaf9",
+            "background4": "#d7eed8",
+            "background5": "#e2e3e4",
+            "background6": "#acd0fc",
+            "background7": "#fdeed6",
+            "background8": "rgba(255, 193, 153, 0.5)",
+            "background9": "#fdd5e7",
+            "background10": "#e6c7e6",
+            "background11": "#def0d9",
+            "background12": "rgba(253, 198, 200, 0.5)",
+            "background13": "#202124"
+        },
+        "dark":{
+            "color1": "rgb(243 153 147)",
+            "color2": "rgb(255 213 153)",
+            "color3": "rgb(166 213 250)",
+            "color4": "rgb(183 223 185)",
+            "color5": "#9aa0a6",
+            "color6": "#3573f0",
+            "color7": "#f3a92f",
+            "color8": "#d23f31",
+            "color9": "#f5539e",
+            "color10": "#bc67bc",
+            "color11": "#65b84d",
+            "color12": "#f5822e",
+            "color13": "#1e1f22",
+            "background1": "#cc352d3d",
+            "background2": "#be8b585d",
+            "background3": "rgb(0 153 255 / 29%)",
+            "background4": "#85d0a34d",
+            "background5": "#4c5257",
+            "background6": "#0c3d88",
+            "background7": "#593905",
+            "background8": "#541812",
+            "background9": "#6a0634",
+            "background10": "#6b2f6b",
+            "background11": "#376629",
+            "background12": "#803a06",
+            "background13": "#c9d1d9"
+        }
+    }
+}
+
 class ColorSchemePlugin extends Plugin {
     config = defaultConfig;
     schemes = null;
@@ -462,21 +524,25 @@ background-color: var(--diy-background13) !important;
     }
 
     async applyScheme(name) {
-        if (name === 'default') {
-            const id = this.config.colorSchemeStyleId;
-            let el = document.getElementById(id);
-            if (el) {
-                el.remove();
-            }
-            this.config.current = name;
-            this.saveConfig();
-            return;
-        }
+        // if (name === 'default') {
+        //     const id = this.config.colorSchemeStyleId;
+        //     let el = document.getElementById(id);
+        //     if (el) {
+        //         el.remove();
+        //     }
+        //     this.config.current = name;
+        //     this.saveConfig();
+        //     return;
+        // }
         let schemes
         //如果this.schemes不存在，则读取json并初始化 schemes
         if (this.schemes === null) {
             schemes = await this.loadSchemeFromFile(name);
-            if (!schemes) {
+            if (!schemes && name === 'default') {
+                schemes = defaultSchemes["schemes"]
+                this.saveScheme("default",schemes)
+            }
+            else if (!schemes) {
                 new Notification({ type: 'error', message: '未找到配色方案' }).show();
                 return;
             }
